4444d54cfad2fbd4351e26d0110c1f91cb38a443
Alfredo Castillo <alfredocastillo100@gmail.com>
2018-05-21 17:39:59 -0500

    Initial Commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Forge-bout-it');
+INSERT INTO burgers (name) VALUES ('Bacon Cheese Burger');
+-- INSERT INTO burgers (name, sleepy) VALUES ('Sylvester', true);
+-- INSERT INTO burgers (name, sleepy) VALUES ('Marilyn Manson', true);
+-- INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO burgers (name) VALUES ('Royale with Cheese');
