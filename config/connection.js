19d21f0ac74a3442741a2d688d188d5a6583c6e9
Alfredo Castillo <alfredocastillo100@gmail.com>
2018-05-22 18:20:26 -0500

    Heroku Changes

--- config/connection.js
+++ config/connection.js
@@ -9,14 +9,25 @@ var connection;
 //   password: "",
 //   database: "burger_db"
 // });
+// if (process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }else {
+//   connection = mysql.createConnection ({
+//     host: 'y5s2h87f6ur56vae.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//     user: 'et58w1g1stufww16',
+//     password: 'sd4qsbjwwh508m6v',
+//     database: 'u9p88lhm03o1093t'
+//   });
+// }
+
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
   connection = mysql.createConnection ({
-    host: 'y5s2h87f6ur56vae.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'et58w1g1stufww16',
-    password: 'sd4qsbjwwh508m6v',
-    database: 'u9p88lhm03o1093t'
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
   });
 }
 // Make connection.
